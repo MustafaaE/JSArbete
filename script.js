e1256b9ebdab2c2bdef749bc3f7c1fa94e11e4ef
Mustafa Elomeiri <mustafaelomeiri@hotmail.se>
2021-11-18 14:14:14 +0100

    script filen

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+/*Hur hemsidan memorerar ens cart när man byter sida.
+
+localStorage.setItem('productcart',JSON.stringify(products)); //spara i lokal databas
+
+let otherProducts = JSON.parse(localStorage.getItem('productcart'));
+
+console.log(otherProducts[0].name); */
